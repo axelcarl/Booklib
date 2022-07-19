1e3c59cfb6aacffca7f8b68a6ed642ab7583d1cd
axelcarl <axelthescarecrow@gmail.com>
2022-07-19 12:09:44 +0200

    Starting to work on authors route

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
+
+
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -23,5 +28,6 @@ db.once('open', () => console.log('db is working'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
