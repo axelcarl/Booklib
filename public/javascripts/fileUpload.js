1f1819735c5f12c42513a00b8fea053166a67dfe
axelcarl <axelthescarecrow@gmail.com>
2022-07-22 22:43:29 +0200

    new filesystem

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  FilePond.registerPlugin(FilePondPluginImagePreview);
+  FilePond.registerPlugin(FilePondPluginImageResize);
+  FilePond.registerPlugin(FilePondPluginFileEncode);
+  FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetHeight: 150,
+    imageResizeTargetWidth: 100,
+  })
+  const inputElement = document.querySelector('input[type="file"]');
+  const pond = FilePond.create(inputElement);
+  FilePond.parse(document.body);
+});  
